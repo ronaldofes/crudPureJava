4cfa42f8e4d4bb952c380d6877008567c1ccf0c5
Ronaldo Ferreira <ronaldofes@gmail.com>
2020-12-13 21:57:05 -0300

    Departament Class

---
+++ src/application/program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Departament;
+
+public class program {
+	
+	public static void main(String[] args) {
+		
+		Departament obj = new Departament (1,"books");
+		
+		System.out.println(obj);		
+		
+	}
+
+}
