5a0f5713bd02a103e1713e4f0cfb7b962269d3b7
Ronaldo Ferreira <ronaldofes@gmail.com>
2020-12-14 00:07:31 -0300

    Insert

--- src/application/program.java
+++ src/application/program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,11 +14,6 @@ public class program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("Test Seller FindById");
-		Seller seller = sellerDao.findById(1);		
-		System.out.println(seller);		
-		
-		
 		System.out.println("\nTest Seller FindByDepartament");
 		Departament departament  = new Departament(2,null);
 		List <Seller> list = sellerDao.findByDepartament(departament);
@@ -25,14 +21,23 @@ public class program {
 		System.out.println(obj);	
 		}
 		
+		System.out.println("\nTest Seller Insert");
+		Seller newSeller = new Seller (1,"Ronaldo","test@tes.com.br",new Date(),5000.,departament);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted New Id = " + newSeller.getId());
+		
+		
+		System.out.println("Test Seller FindById");
+		Seller seller = sellerDao.findById(1);		
+		System.out.println(seller);		
+		
+				
 		System.out.println("\nTest Seller FindAll");
 		list = sellerDao.findAll();
 		for (Seller obj: list) {
 		System.out.println(obj);	
 		}
-		
-		
-		
+			
 	}
 
 }
